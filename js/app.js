fa5ea658fbecdd1d720dd43a44df1755831de614
brendamesias <brendamesiasvillar@gmail.com>
2018-02-07 18:16:41 -0500

    cambios

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  /*
+  let begin = () => {
+    let arrImages = [
+      {id:'0', name: 'Aracely Gutarra', uri: 'AracelyGutarra.png'},
+      {id:'1', name: 'Brenda Mesias', uri: 'BrendaMesias.png'},
+      {id:'2', name: 'daniela Pariona', uri: 'danielaPariona.png'},
+      {id:'3', name: 'Elizabeth Alcala', uri: 'ElizabethAlcala.png'},
+      {id:'4', name: 'jennifer Carmen', uri: 'jenniferCarmen.png'},
+      {id:'5', name: 'karina Ramirez', uri: 'karinaRamirez.png'},
+      {id:'6', name: 'Lesly Nomberto', uri: 'LeslyNomberto.png'},
+      {id:'7', name: 'Ornella Ysabel Campos', uri: 'OrnellaYsabelCampos.png'},
+      {id:'8', name: 'Pamela de la Cruz', uri: 'PameladelaCruz.png'},
+      {id:'9', name: 'yanira Arenazas', uri: 'yaniraArenazas.png'}
+    ];
+    */
+
+
+
+
+
+
+});
